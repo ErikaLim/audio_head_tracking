d23ca5413cf9f5bd79fd7bb7b887739781e82870
ErikaLim <Erika.megan.lim@gmail.com>
2015-01-23 14:29:29 -0800

    sound and headtracker

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var context = new AudioContext(); // Create audio container
+
+oscillator = context.createOscillator(); // Create sound source
+gainNode = context.createGain(); // Create boost pedal
+oscillator.connect(gainNode); // Connect bass guitar to boost pedal
+oscillator.connect(context.destination); // Connect sound to speakers
+gainNode.gain.value = 0.3; // Set boost pedal to 30 percent volume
+oscillator.start(); // Generate sound instantly
+
+var videoInput = document.getElementById('inputVideo');
+var canvasInput = document.getElementById('inputCanvas');
+
+var htracker = new headtrackr.Tracker();
+htracker.init(videoInput, canvasInput);
+htracker.start();
+
+
+
+document.addEventListener('headtrackrStatus',
+function (event) {
+  if (event.status == "getUserMedia") {
+    alert("getUserMedia is supported!");
+  }
+}
+);
