244334f9fe6c41f66a45846eba3bd856c2081dea
David Dang <dtudang@gmail.com>
2015-01-23 15:41:58 -0800

    finished

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ gainNode = context.createGain(); // Create boost pedal
 oscillator.connect(gainNode); // Connect bass guitar to boost pedal
 oscillator.connect(context.destination); // Connect sound to speakers
 gainNode.gain.value = 0.3; // Set boost pedal to 30 percent volume
+oscillator.frequency.value = 100; // Frequency in hertz
 oscillator.start(); // Generate sound instantly
 
 var videoInput = document.getElementById('inputVideo');
@@ -16,10 +17,8 @@ htracker.start();
 
 
 
-document.addEventListener('headtrackrStatus',
+document.addEventListener('facetrackingEvent',
 function (event) {
-  if (event.status == "getUserMedia") {
-    alert("getUserMedia is supported!");
-  }
+  oscillator.frequency.value = event.x * 2;
 }
 );
